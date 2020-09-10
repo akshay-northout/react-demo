8dc769a6ee2e4604bd7b9c911fe08635943aeae1
akshay-northout <akshay-northout@users.noreply.github.com>
2020-09-10 10:24:04 +0000

    deploy: 9ba757231b9b770b3a30f5decbbbb56103f8c495

---
+++ precache-manifest.a1cd0a33fa5627c3f5aa3648d6e01b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956b2f455a7e5d1741e3e273e39a6f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56250d3ff089feded99b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9e9f67bad663953fe5d",
+    "url": "/static/js/2.d39f5f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d39f5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56250d3ff089feded99b",
+    "url": "/static/js/main.1d78f26e.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
